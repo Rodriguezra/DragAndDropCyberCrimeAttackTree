fbac602172418d818ab3226b4fb94fef5c8bfa3d
rodriguezra <raul.rodriguez@ufl.edu>
2024-10-18 02:01:16 -0400

    text changes

--- script.js
+++ script.js
@@ -341,15 +341,16 @@ function draw() {
     image(ChipImg, imgX, imgY);
     scale(1 / (.00016 * width));
 
-    // Define the text content
-    // Set text properties
-    noStroke();
-    strokeWeight(1);
+    // center image rectangle
     fill(255);
     rectMode(CENTER);
+    rect(width / 2, height / 2, width - 400, height - 270, 10);
+
+    // game text
+    noStroke();
+    strokeWeight(1);
+    rectMode(CENTER);
     rect(width / 2, 60, 1000, 100, 10);
-    rectMode(CORNER);
-    rect(200, 120, width - 400, height - 270, 10);
 
     // Display text content
     textSize(18);
@@ -517,13 +518,13 @@ function showInstructionScreen() {
 
   // instructions text
   textSize(22); // Adjusted font size
-  textAlign(CENTER, CENTER); // Adjusted text alignment
+  textAlign(CENTER, TOP); // Adjusted text alignment
 
   // Additional text
   fill(color(0));
   textFont(font2); // change font
   let textX = width / 2; // X position for the additional text
-  let textY = height / 1.5; // Starting Y position for the additional text
+  let textY = height / 3; // Starting Y position for the additional text
   let textLeading = 24; // Line spacing
   let textWidth = 550; // Width of the text block
   let additionalText = "Your objective is to correctly place each card into its designated slot.\n\nTo play, click and hold on a card, then drag it to the numbered slot where you think it belongs.\nRelease the mouse to drop the card into place.\n\nRemember, each card has a specific slot it must occupy.\nWhen all cards have been placed, you'll see an option to check your answers.\n\nIf you're correct, you'll have the option to play again.";
